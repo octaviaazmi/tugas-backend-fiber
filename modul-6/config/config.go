216c061ca38648c3995efb36ecaf6ec8b7315d20
octaviaazmi <octaviaazmi21@gmail.com>
2026-09-24 18:53:29 +0700

    feat: tambah konfigurasi dan koneksi database

---
+++ modul-6/config/config.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"os"
+	"strconv"
+)
+
+func GetEnv(key, fallback string) string {
+	if v := os.Getenv(key); v != "" {
+		return v
+	}
+	return fallback
+}
+
+func GetEnvInt(key string, fallback int) int {
+	if v := os.Getenv(key); v != "" {
+		if n, err := strconv.Atoi(v); err == nil {
+			return n
+		}
+	}
+	return fallback
+}
